e279c192a9d111c6e3db8159a209461773045c9e
James Reid <jreidmke@gmail.com>
2020-12-17 10:51:27 -0600

    completed app.js functionality

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const btn = $('button');
+const btn = $('#submit');
 const input = $('input');
-const factList = $('ul')
+const factList = $('ul');
+const clear = $('#clear');
 
-async function fetchData(num) {
-    const baseURL = `http://numbersapi.com/${num}/trivia?json`;
+async function fetchData() {
+    const nums = input.val()
+    const baseURL = `http://numbersapi.com/${nums}/trivia?json`;
     const resp = await axios.get(baseURL);
-    console.log(resp.data.text);
-    factList.append(`<li>${resp.data.text}</li>`);
+    console.log(nums.includes(','));
+    if(!nums.includes(',')) {
+        factList.append(`<li>${resp.data.text}</li>`)
+    } else {
+        for(let fact in resp.data) {
+            factList.append(`<li>${resp.data[fact]}</li>`);
+        }
+    }
+    input.val('');
 }
 
 btn.on('click', function(e) {e.preventDefault()});
-btn.on('click', () => fetchData(input.val()));
+btn.on('click', fetchData);
+clear.on('click', () => factList.empty());
