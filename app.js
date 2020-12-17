f363a314784cfc2b0038900969733c1123431b11
James Reid <jreidmke@gmail.com>
2020-12-17 10:55:45 -0600

    added error logging

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ const clear = $('#clear');
 async function fetchData() {
     const nums = input.val()
     const baseURL = `http://numbersapi.com/${nums}/trivia?json`;
-    const resp = await axios.get(baseURL);
-    console.log(nums.includes(','));
-    if(!nums.includes(',')) {
-        factList.append(`<li>${resp.data.text}</li>`)
-    } else {
-        for(let fact in resp.data) {
-            factList.append(`<li>${resp.data[fact]}</li>`);
-        }
+    try {
+        const resp = await axios.get(baseURL);
+        if(!nums.includes(',')) {
+            factList.append(`<li>${resp.data.text}</li>`)
+        } else {
+            for(let fact in resp.data) {
+                factList.append(`<li>${resp.data[fact]}</li>`);
+            }
+        } 
+    }
+    catch (error) {
+        console.log(error);
+        alert(error + ". Please make sure your numbers are seperated with commas, not spaces (ex: 1,2,4,85)");
     }
     input.val('');
 }
