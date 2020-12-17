350694d1c5e8436186281ee9657490ddb96d2036
James Reid <jreidmke@gmail.com>
2020-12-17 11:47:10 -0600

    completed card draw functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Number facts stuff.
+
 const btn = $('#submit');
 const input = $('input');
 const factList = $('ul');
@@ -21,12 +23,38 @@ async function fetchData() {
         alert(error + ". Please make sure your numbers are seperated with commas, not spaces (ex: 1,2,4,85)");
     }
     input.val('');
-}
+};
 
 btn.on('click', function(e) {e.preventDefault()});
 btn.on('click', fetchData);
 clear.on('click', () => factList.empty());
 
-function addOne(x) {
-    return x + 1; 
+//Card stuff
+
+const cardBtn = $('#draw-card');
+const cardImg = $("#cardImg");
+
+$(document).ready(shuffleDeck);
+
+let deckId;
+
+async function shuffleDeck() {
+    const resp = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
+    deckId = resp.data.deck_id;
 }
+
+async function drawCard() {
+    try { 
+        const resp = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+        if(resp.data.remaining === 0) {
+            alert('All Out! To restart, refresh the page.');
+        }
+        const card = resp.data.cards[0];
+        cardImg.attr('src', card.image);
+    } 
+    catch (error) {
+        alert('Something went wrong!');
+    }
+}
+
+cardBtn.on('click', drawCard);
